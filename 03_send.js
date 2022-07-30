44b365bf596955bf6ece26096a98fec9d653cbba
Okan Demir <huseyinokand@gmail.com>
2022-07-30 11:14:26 +0300

    Send feature is implemented

---
+++ 03_send.js
@@ -0,0 +1,24 @@
+const { Wallet, getDefaultProvider, utils } = require("ethers");
+
+const { readFileSync } = require("fs");
+
+async function main(_receiverAddress, _ethAmount) {
+  const network = "rinkeby";
+  const provider = getDefaultProvider(network);
+  const accountRawData = readFileSync("account.json", "utf-8");
+  const accountData = JSON.parse(accountRawData);
+  const privateKey = Object.values(accountData.privateKey);
+  const signer = new Wallet(privateKey, provider);
+  const transaction = await signer.sendTransaction({
+    to: _receiverAddress,
+    value: utils.parseEther(_ethAmount),
+  });
+
+  console.log(transaction);
+}
+main(process.argv[2], process.argv[3])
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
